d72624fef9608e14d125f5a49ba9a24ba18ec8a2
Jean Pating <jeancyrille2002@gmail.com>
2024-03-23 12:00:13 +0800

    added employee login

--- conns/server.php
+++ conns/server.php
@@ -1,5 +1,4 @@
 <?php
-
 // initializing variables
 $username = "";
 $errors = array(); 
@@ -7,118 +6,45 @@ $errors = array();
 // connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'employeesdb');
 
-
-
 // LOGIN USER
-if (isset($_POST['oksbut'])) 
-{
-  $username = mysqli_real_escape_string($db, $_POST['uname']);
-  $password = mysqli_real_escape_string($db, $_POST['upass']);
+if (isset($_POST['oksbut'])) {
+    $username = mysqli_real_escape_string($db, $_POST['uname']);
+    $password = mysqli_real_escape_string($db, $_POST['upass']);
 
-  if (empty($username)) 
-  {
-  	array_push($errors, "Username is required");
-  }
-  if (empty($password)) 
-  {
-  	array_push($errors, "Password is required");
-  }
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
 
-  if (count($errors) == 0) 
-  {
-  	
-  	$query1 = "SELECT * FROM users WHERE user_email='$username' AND user_pwd='$password'";
-  	$results1 = mysqli_query($db, $query1);
+    if (count($errors) == 0) {
+       	// Check admin table
+		$query1 = "SELECT * FROM users WHERE user_email='$username' AND user_pwd='$password'";
+		$results1 = mysqli_query($db, $query1);
 
-  	if (mysqli_num_rows($results1) == 1) 
-	{
+        // Check employee table
+        $query_employee = "SELECT * FROM emp_acc WHERE emp_email='$username' AND emp_pwd='$password'";
+        $results_employee = mysqli_query($db, $query_employee);
 
+		if (mysqli_num_rows($results1) == 1) {
+		// Admin login
 		session_start();
 		$_SESSION['username1'] = $username;
 		header('location: admin.php');
-
-	}
-    else
-    {
-        header("location: login.php?wup=Wrong Username / Password&user=".$username."");
-    }
-
-  }
-
-}
-
-
-
-
-
-
-
-// LOGIN USER
-/*
-if (isset($_POST['oksbut'])) 
-{
-  $username = mysqli_real_escape_string($db, $_POST['uname']);
-  $password = mysqli_real_escape_string($db, $_POST['upass']);
-
-  if (empty($username)) 
-  {
-  	array_push($errors, "Username is required");
-  }
-  if (empty($password)) 
-  {
-  	array_push($errors, "Password is required");
-  }
-
-  if (count($errors) == 0) 
-  {
-  	
-  	$query1 = "SELECT * FROM users WHERE user_email='$username' AND user_pwd='$password'";
-  	$results1 = mysqli_query($db, $query1);
-
-  	if (mysqli_num_rows($results1) == 1) 
-	{
-
-		session_start();
-		$_SESSION['username1'] = $username;
-		header('location: Navbar/MainPOS/POS.php');
-
-	}
-	elseif(mysqli_num_rows($results1) == 0)
-	{
-
-		$query2 = "SELECT * FROM user_Manager WHERE user_email='$username' AND user_pwd='$password'";
-		$results2 = mysqli_query($db, $query2);
   
-		if (mysqli_num_rows($results2) == 1) 
-		{
-
-		  session_start();
-		  $_SESSION['username2'] = $username;
-		  header('location: Navbar/MainPOS/POS.php');
-
-		}
-		elseif(mysqli_num_rows($results2) == 0)
-		{
-
-			$query3 = "SELECT * FROM user_Cashier WHERE user_email='$username' AND user_pwd='$password'";
-			$results3 = mysqli_query($db, $query3);
-	  
-			if (mysqli_num_rows($results3) == 1) 
-			{
-
-			  session_start();
-			  $_SESSION['username3'] = $username;
-			  header('location: Navbar/MainPOS/POS.php');
-
-			}
-			else
-			{
-				header("location: login.php?wup=Wrong Username / Password&user=".$username."");
-			}
-		}
+        } elseif (mysqli_num_rows($results_employee) == 1) {
+            // Employee login
+			$row = mysqli_fetch_assoc($results_employee); 
+			$employeeId = $row['emp_id']; // Get employee ID
+			session_start();
+			$_SESSION['username'] = $username;
+			header("location: employee_dashboard.php?emp_id=$employeeId");
+			exit(); 
+        } else {
+            // Invalid username/password
+            header("location: login.php?wup=Wrong Username / Password&user=".$username."");
+        }
     }
-
-	}
 }
-*/
 ?>
